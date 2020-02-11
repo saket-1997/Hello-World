ea13d3a601e2b4d1617e56f80659809c4f967560
saket-1997 <saketm1994@gmail.com>
2020-02-11 23:00:40 +0530

    FOLDER ADDED

---
+++ dummy/Usecase2.ps1
@@ -0,0 +1,54 @@
+﻿#Taking input from user
+$input=Read-Host -Prompt "Enter the Type of Utilization you want(cpu,disk,memory)"
+#Initializing an array.
+$cpu=@(0..4)
+$memory=@(0..4)
+#intializing the values.
+$avg=0
+$avg2=0
+$RoundMemory=@(0..4)
+#Calculating CPU Utilization.
+if($input -match "cpu")
+  {
+    Write-Host "Calculating CPU Utilization...."
+   for($i=0;$i -lt 5 ;$i++)
+     { 
+      $cpu[$i]=(Get-WmiObject -Class win32_processor).LoadPercentage[0]
+      Start-Sleep -seconds 1
+      $avg+=$cpu[$i]
+     }
+   $avg_cpu_util=$avg/5
+   Write-Host "Average Percentage of CPU Utilization is $avg_cpu_util"
+  }
+
+#Calculating Disk Utilization
+elseif($input -match "disk")
+{
+   $drive=Read-Host "Enter the Drive for which you want to calculate Disk Utilization:"
+   Write-Host "Calculating Disk Utilization...."
+   $Disk = Get-WMIObject Win32_Logicaldisk -filter "deviceid='$drive'"
+   $FreeSpacePer = ((($Disk.Freespace /1Gb -as [float]) / ($Disk.Size / 1Gb -as [float]))*100) -as [int]
+   $SpaceUsePer = 100 - $FreeSpacePer
+   Write-Host "Free Space Percentage of $drive Drive is $FreeSpacePer"
+   Write-Host "Space Used Percentage of $drive Drive is $SpaceUsePer"
+}
+
+#calculating Memory Utilization
+elseif($input -match "memory")
+{
+Write-Host "Calculating Memory Utilization...."
+  for($i=0;$i -lt 5 ;$i++)
+   {
+    $ComputerMemory = Get-WmiObject -Class win32_operatingsystem -ErrorAction Stop
+    $memory[$i] = ((($ComputerMemory.TotalVisibleMemorySize - $ComputerMemory.FreePhysicalMemory)*100)/ $ComputerMemory.TotalVisibleMemorySize)
+    $RoundMemory[$i] = [math]::Round($Memory[$i], 2)
+    Start-Sleep -Seconds 1
+    $avg2+=$RoundMemory[$i]
+   }
+   $avg_mem_util=$avg2/5
+   Write-Host "Average Percentage of Memory Utilization is $avg_mem_util"
+}
+else
+{
+ Write-host "Please Enter a Valid Type! Your Type doesn't match."
+}
---
+++ dummy/Usecase3.ps1
@@ -0,0 +1,45 @@
+﻿#-------------------Taking Input from User along with validation------------------------
+$x=Read-host "Enter your Name:" 
+$s=Read-Host "Enter your Sap-id:"
+$y=Read-host "Hello $x, Enter the number of leaves you want:"
+while($y -notmatch "^[\d\.]+$")
+{
+    "Please enter Leaves in Integer"
+     $y=Read-host "Hello $x, Enter the number of leaves you want:"
+}
+$z=Read-host "Hello $x. Enter the number of Half-day you want:"
+while($z -notmatch "^[\d\.]+$")
+{
+    "Please enter half day in integer"
+     $z=Read-host "Hello $x. Enter the number of Half-day you want:"
+}
+while(8 -lt $y)
+{
+    Write-Host "You can not apply Leaves for more than 8 Days. Please enter the input again."
+    $y=Read-host "Hello $x, Enter the number of leaves you want:"
+}
+while(8 -lt $z)
+{
+    Write-Host "You can not apply Half day for more than 8 Days.Please enter the input again."
+    $z=Read-host "Hello $x. Enter the number of Half-day you want:"
+}
+#----------------Calculating acting hours--------------------------
+$month=30
+$weekend=8
+$halfday=5 * $z
+$acting_hours_day=9 *($month-$weekend-$z-$y) + $halfday
+$sum+=$acting_hours_day
+$h1=@{Acting_hours=$acting_hours_day;Sap_id=$s;Name=$x}
+$ob1 = New-Object -TypeName PSCustomObject -Property $h1
+$ob1 | Export-csv C:\Users\saket.maheshwari\Desktop\acting.csv -NoTypeInformation -Append
+$Timesheet=Import-Csv C:\Users\saket.maheshwari\Desktop\acting.csv
+for($i=0;$i -le $Timesheet.length;$i++)
+{
+    $hash=$Timesheet| Where-Object {$_.Name[$i] -ne "#"} | Export-Csv C:\Users\saket.maheshwari\Desktop\acting.csv -NoTypeInformation
+}
+$new=Import-Csv C:\Users\saket.maheshwari\Desktop\acting.csv
+$sheet=($new| Measure-Object 'Acting_hours' -Sum).sum
+$h2=@{Acting_hours=$sheet;Sap_id="#####";Name="#####"}
+$ob2 = New-Object -TypeName PSCustomObject -Property $h2
+$ob2| Export-csv C:\Users\saket.maheshwari\Desktop\acting.csv -NoTypeInformation -Append -Force
+
